5186b469e18d24e8263027e9cfea10148f92f050
Christian Vladimir Sucuzhanay Arevalo <sukuzhanay@gmail.com>
2022-03-29 21:40:57 +0200

    Added about component

--- 001-tabs/002-angular-crash-course/src/app/app.module.ts
+++ 001-tabs/002-angular-crash-course/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AboutComponent } from './pages/about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule
