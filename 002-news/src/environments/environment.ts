83b71c99767f2eaed8f269402d8e4ca7a9d20aed
Christian Vladimir Sucuzhanay Arevalo <sukuzhanay@gmail.com>
2022-03-30 02:30:54 +0200

    added apiKey in enviroment.ts

--- 002-news/src/environments/environment.ts
+++ 002-news/src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false, 
+  apiKey : 'a7169ca270f543c79e7bd8f49a513035'
 };
 
 /*
