e0bdccd3cb04d74f57d8903b64c0517f5c422ece
Christian Vladimir Sucuzhanay Arevalo <sukuzhanay@gmail.com>
2022-03-30 13:37:57 +0200

    import and export Articles

--- 002-news/src/app/components/components.module.ts
+++ 002-news/src/app/components/components.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
-
-
-
+import { ArticleComponent } from './article/article.component';
+import { ArticlesComponent } from './articles/articles.component';
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    IonicModule // obligatorio para poder usar ion-component
+    IonicModule, // obligatorio para poder usar ion-component
+    ArticleComponent, ArticlesComponent  // creados para reutilizar
+  ],
+  exports:[
+    ArticlesComponent,
   ]
 })
 export class ComponentsModule { }
