f66019373e6add4195c4f5317ad1302cf98f08b5
Christian Vladimir Sucuzhanay Arevalo <sukuzhanay@gmail.com>
2022-03-30 04:15:20 +0200

    Refact code, observables, articles page 15

--- 002-news/src/app/services/news.service.ts
+++ 002-news/src/app/services/news.service.ts
@@ -1,6 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
+import { Article, NewsResp } from '../interfaces/index';
+import { map } from "rxjs/operators";
 const apiKey = environment.apiKey;
 @Injectable({
   providedIn: 'root'
@@ -9,12 +12,10 @@ export class NewsService {
 
   constructor(private http: HttpClient) { }
 
-  getTopNews(){
-    return this.http.get(`https://newsapi.org/v2/top-headlines?sources=techcrunch`,{
-      params :{
-        apiKey : apiKey
-      }
-    })
+  getTopNews():Observable<Article[]>{
+    return this.http.get<NewsResp>(`https://newsapi.org/v2/top-headlines?sources=techcrunch`,{
+      params :{apiKey}
+    }).pipe(map(( { articles } )=> articles));
   } 
 }
  
